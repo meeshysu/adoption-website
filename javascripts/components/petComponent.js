d2c9885d074a46bd84f58c97333b7827dca07483
Michelle Beshears <meeshysu@gmail.com>
2018-10-06 11:54:55 -0500

    photos are same size now and aligned

--- javascripts/components/petComponent.js
+++ javascripts/components/petComponent.js
@@ -14,19 +14,18 @@ const getPetz = () => {
 const petsBuilder = (petsArray) => {
     let domString = '';
     petsArray.forEach((pet) => { 
-    domString += `<div class="pet-card">`
-    domString += `<div id="pet-card" class="petCard card border-success mb-3 col-2" style="max-width: 18rem;">`;
-    domString +=     `<h4 class="card-title">${pet.name}</h4>`;
-    domString +=     `<h6 class="colorOfPet">${pet.color}</h6>`;
-    domString +=     `<img class="card-img-top" src="${pet.imageUrl}" alt="${pet.name}">`;
-    domString +=     `<p class="card-text">${pet.specialSkill}</p>`;
-    domString +=    `<div class="card-footer bg-transparent border-success">${pet.type}</div>`;
+    domString +=    `<div class="card border-success m-2" style="max-width: 22rem;">`;
+    domString +=      `<div class="card-header bg-transparent border-success">${pet.name}</div>`;
+    domString +=        `<div class="card-body text-success">`;
+    domString +=          `<img class="petPic" src="${pet.imageUrl}" alt="${pet.name}">`;
+    domString +=          `<p class="card-text">${pet.specialSkill}</p>`;
+    domString +=           `<h6 class="colorOfPet">${pet.color}</h6>`;
+    domString +=        `</div>`
+    domString +=       `<div class="card-footer bg-transparent border-success">${pet.type}</div>`;
     domString +=   `</div>`
-    domString +=   `</div>`;
 })
     printToDom(domString);
 };
 
 
 export {petsBuilder, setPets, getPetz, pets};
-
