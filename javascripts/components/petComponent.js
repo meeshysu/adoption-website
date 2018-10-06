8cfb0afaf0a4dd59f3a43821ad685bc4a17b8768
Michelle Beshears <meeshysu@gmail.com>
2018-10-06 13:32:46 -0500

    cards finished

--- javascripts/components/petComponent.js
+++ javascripts/components/petComponent.js
@@ -14,14 +14,14 @@ const getPetz = () => {
 const petsBuilder = (petsArray) => {
     let domString = '';
     petsArray.forEach((pet) => { 
-    domString +=    `<div class="card border-success m-2" style="max-width: 22rem;">`;
-    domString +=      `<div class="card-header bg-transparent border-success">${pet.name}</div>`;
-    domString +=        `<div class="card-body text-success">`;
+    domString +=    `<div class="card m-2" style="max-width: 22rem;">`;
+    domString +=      `<div class="card-header">${pet.name}</div>`;
+    domString +=        `<div class="card-body">`;
     domString +=          `<img class="petPic" src="${pet.imageUrl}" alt="${pet.name}">`;
     domString +=          `<p class="card-text">${pet.specialSkill}</p>`;
     domString +=           `<h6 class="colorOfPet">${pet.color}</h6>`;
     domString +=        `</div>`
-    domString +=       `<div class="card-footer bg-transparent border-success">${pet.type}</div>`;
+    domString +=       `<div id="type${pet.type}">${pet.type}</div>`;
     domString +=   `</div>`
 })
     printToDom(domString);
